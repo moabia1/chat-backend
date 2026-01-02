477831788ae32c405906da4a3dc7d7d267b5f396
moabia1 <mdmoabia81@gmail.com>
2026-01-02 13:32:59 +0530

    updates of cors

--- mail/src/index.ts
+++ mail/src/index.ts
@@ -1,11 +1,14 @@
 import express from "express"
 import dotenv from "dotenv"
+import cors from "cors"
 import { sendOtpConsumer } from "./consumer.js";
 dotenv.config()
 
 sendOtpConsumer()
 const app = express();
-
+app.use(cors({
+  origin:"https://chat2-peer.vercel.app"
+}))
 
 
 app.listen(5001, () => {
